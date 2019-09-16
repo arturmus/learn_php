b6d2e2095ba4f1557ff6e3181ba912cb859c37d7
arturmus <55016717+arturmus@users.noreply.github.com>
2019-09-16 14:41:02 +0300

    avers_revars

---
+++ text.php
@@ -0,0 +1,19 @@
+<?php
+
+$str = 'GOD IS LOVE !';
+$length = strlen($str);
+for ($i = 0; $i < $length; $i++) {
+    echo $i, ' ', substr($str, $i, 1), PHP_EOL;
+}
+
+echo 'END', PHP_EOL;
+
+
+$str = 'GOD IS LOVE !';
+$length = strlen($str);
+for ($i = 0; $i > -$length; $i--) {
+    echo $i, ' ', substr($str, $i, 1), PHP_EOL;
+}
+
+echo 'END';
+
