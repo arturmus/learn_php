cf74b95585ae0900172f6955ec239cc463f6f5a4
arturmus <55016717+arturmus@users.noreply.github.com>
2019-09-18 12:12:35 +0300

    Update text.php

--- text.php
+++ text.php
@@ -11,9 +11,10 @@ echo 'END', PHP_EOL;
 
 $str = 'GOD IS LOVE !';
 $length = strlen($str);
-for ($i = 0; $i > -$length; $i--) {
+
+$str = 'GOD IS LOVE !';
+$length = strlen($str);
+for ($i = $length; $i >= 0; $i--) {
     echo $i, ' ', substr($str, $i, 1), PHP_EOL;
 }
-
 echo 'END';
-
