6fab669382105c31d367263205f2b8f3bc559f4f
arturmus <55016717+arturmus@users.noreply.github.com>
2019-10-17 12:27:03 +0300

    Update time.php

--- time.php
+++ time.php
@@ -1,10 +1,11 @@
 <?php
   
- $x = time() / (365*24*60*60 + 6*60*60) + 1970;
+ 
+  $x = time() / (365*24*60*60 + 6*60*60) + 1970;
 
-    var_damp(round ($x , 0));
-     echo 'Year: '. $x . PHP_EOL;
+ 
+     echo 'Year: '.  floor($x) . PHP_EOL;
      
      $y = time() / (24*60*60);
      
-     echo 'Day:' . $y . PHP_EOL;
+     echo 'Day:' . floor($y) . PHP_EOL;
