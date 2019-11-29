bfaf458e366559b8623d52f30100bff77db3752b
arturmus <55016717+arturmus@users.noreply.github.com>
2019-11-29 22:24:12 +0200

    Update bubble.php

--- bubble.php
+++ bubble.php
@@ -1,18 +1,14 @@
 <?php
 
-$array = array(25, 8, 3, 14);
+$massiv = array(25, 8, 3, 14);
 
-for ($j = 0; $j < count($array) - 1; $j++) {
+foreach($massiv as $key => $item) {
     
-    for ($i = 0; $i < count($array) - 1 -$j; $i++) {
-        
-        if ($array [i] > $array[i + 1]) {
-            
-            $bubble = $array [$i + 1];
-            $array[$i + 1] = $array[$i];
-            $array [$i] = $bubble;
-        }
+    if($item > $massiv[$key + 1]) {
+        $tmp = $massiv[$key + 1];
+        $massiv[$key] = $tmp;
+        $massiv[$key + 1] = $item;
     }
 }
 
-var_dump ($array);
+var_dump($massiv);
