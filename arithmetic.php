a073f8b530fc978f49efc1b6dc0e52185c7d338a
arturmus <55016717+arturmus@users.noreply.github.com>
2020-04-05 14:31:36 +0300

    amount

---
+++ arithmetic.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <body>
+            <h1>Арифметика</h1>
+            <h2>
+                <?php echo 'a+b=' , $_GET ['a'] + $_GET ['b']; ?>
+            </h2>
+        </body>
+    </head>
+</html>
+    
